7699be289596e743e852367380d31a52d0190660
jenpato <jenny.paterson1@gmail.com>
2025-08-16 21:01:28 +1000

    Rendered the word meanings

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "./App.css";
 import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
       <main>
         <Dictionary />
       </main>
+      <footer className="App-footer">
+        <small>Coded by Jen Paterson</small>
+      </footer>
     </div>
   );
 }
-
-export default App;
