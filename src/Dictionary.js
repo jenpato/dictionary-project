d25b165510fce87dea2d2180b831cf1b568299df
jenpato <jenny.paterson1@gmail.com>
2025-08-20 16:24:41 +1000

    Integrated photos into the results

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,22 +1,37 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
-export default function Dictionary() {
+export default function Dictionary(props) {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionResponse(response) {
     setResults(response.data[0]);
   }
 
-  function search(event) {
-    event.preventDefault();
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
 
+  function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
 
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionResponse);
+
+    let pexelsApiKey =
+      "GgamP9aCT50mFsXv6lOV2au9VCSwXOVNsVI6RbQ9DDQIaNBIzP0xHoRv";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
   }
 
   function handleKeywordChange(event) {
@@ -27,12 +42,13 @@ export default function Dictionary() {
     <div className="Dictionary">
       <section>
         <h1>What word do you want to look up?</h1>
-        <form onSubmit={search}>
+        <form onSubmit={handleSubmit}>
           <input type="search" onChange={handleKeywordChange} />
         </form>
         <div className="hint">suggested words: sunset, table, wine, car...</div>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
